4b83a73984918c844ceed2267c620d0dc1cf0d47
Edward Chung <edwardchung2024@gmail.com>
2022-04-03 05:56:25 -0400

    dependencies added

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
-import { Card, CardMedia, CardContent, CardActions, Typography, IconBotton } from '@mui/material'
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@mui/material'
 import { AddShoppingCart } from '@mui/icons-material'
 
-const Product = () => {
-  return (
-      <Card className = {classes.root}>
-          <CardMedia className = {classes.media} image='' title = {product.name} />
-      </Card>
-  )
+import useStyles from './styles';
+
+const Product = ({ product }) => {
+    const classes = useStyles();
+        return (
+            <Card className = {classes.root}>
+                <CardMedia className = {classes.media} image='' title = {product.name} />
+                <CardContent>
+                    <div className = {classes.cardContent}>
+                        <Typography variant = "h5" gutterBottom>
+                            {product.name}
+                        </Typography>
+                        <Typography variant = "h5" >
+                            {product.price}
+                        </Typography>
+                    </div>
+                    <Typography variant = "h2" color="textSecondary">
+                        {product.description}
+                    </Typography>
+                </CardContent>
+                <CardActions disableSpacing className={classes.cardActions}>
+                    <IconButton aria-label="Add to Cart">
+                        <AddShoppingCart />
+                    </IconButton>
+                </CardActions>
+            </Card>
+        )
 }
 
 export default Product
