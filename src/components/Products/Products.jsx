bc9cb35275c2118ea63b9f4bbb7ecd97718088e2
Edward Chung <edwardchung2024@gmail.com>
2022-04-03 06:03:07 -0400

    add images

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,8 +4,8 @@ import {Grid} from '@mui/material';
 import Product from './Product/Product';
 
 const products = [
-    {id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5'},
-    {id: 2, name: 'Macbook', description: 'Apple macbook.', price: '$10'}
+    {id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5', image: 'https://media.istockphoto.com/photos/modern-sport-shoes-picture-id623270836?k=20&m=623270836&s=612x612&w=0&h=C0WdoMeoHYugJy8mVgrTl8p1U8DltiZ25AzzjGY05GA='},
+    {id: 2, name: 'Macbook', description: 'Apple macbook.', price: '$10', image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/macbook-air-space-gray-select-201810?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1633027804000'}
 ];
 
 const Products = () => {
