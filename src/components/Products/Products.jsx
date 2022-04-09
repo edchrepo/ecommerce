f526d46648e5c9643d70ee12b8cc92f454c7705b
Edward Chung <edwardchung2024@gmail.com>
2022-04-09 15:16:34 -0400

    style changes

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {Grid} from '@mui/material';
 
 import Product from './Product/Product';
+import styles from './styles';
+import useClasses from '../../hook';
 
 const products = [
     {id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5', image: 'https://media.istockphoto.com/photos/modern-sport-shoes-picture-id623270836?k=20&m=623270836&s=612x612&w=0&h=C0WdoMeoHYugJy8mVgrTl8p1U8DltiZ25AzzjGY05GA='},
@@ -9,8 +11,10 @@ const products = [
 ];
 
 const Products = () => {
+    const classes = useClasses(styles);
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify = "center" spacing = {4}>
                 {products.map((product) => (
                     <Grid item key ={product.id} xs = {12} sm = {6} md = {4} lg = {3}>
--- src/components/Products/styles.js
+++ src/components/Products/styles.js
@@ -2,7 +2,7 @@
 // import makeStyles from '@mui/styles/makeStyles';
 
 const useStyles = theme => ({
-  toolbar: theme.mixins.toolbar,
+  // toolbar: theme.mixins.toolbar,
   content: {
     flexGrow: 1,
     // backgroundColor: theme.palette.background.default,
