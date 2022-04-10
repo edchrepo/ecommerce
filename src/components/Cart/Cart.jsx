31b7e570594046752becb714228941732c29e071
Edward Chung <edwardchung2024@gmail.com>
2022-04-10 19:53:49 -0400

    finished cart layout

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import { Container, Typography, Button, Grid } from '@mui/material'
+import React from 'react';
+import { Container, Typography, Button, Grid } from '@mui/material';
 
 import styles from './styles';
 import useClasses from '../../hook';
+import CartItem from './CartItem/CartItem';
 
 const Cart = ({ cart }) => {
   const classes = useClasses(styles);
@@ -18,13 +19,13 @@ const Cart = ({ cart }) => {
         <Grid container spacing={3}>
             {cart.line_items.map((item) => (
                 <Grid item xs={12} sm={4} key={item.id}>
-                    <div>{item.name}</div>
+                    <CartItem item={item}/>
                 </Grid>
             ))}
         </Grid>
         <div className={classes.cardDetails}>
             <Typography variant="h4">
-                Subtotal:{ cart.subtotal.formatted_symbol }
+                Subtotal: { cart.subtotal.formatted_with_symbol }
             </Typography>
             <div>
                 <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary">
@@ -41,11 +42,11 @@ const Cart = ({ cart }) => {
 
   if(!cart.line_items)
     return 'Loading ...';
-  
+
   return (
         <Container>
             <div className={classes.toolbar} />
-            <Typography className={classes.title} variant="h3">
+            <Typography className={classes.title} variant="h3" gutterBottom>
                 Your Shopping Cart
             </Typography>
             { !cart.line_items.length ? <EmptyCart /> : <FilledCart />}
---
+++ src/components/Cart/CartItem/styles.js
@@ -0,0 +1,18 @@
+const useStyles = {
+  media: {
+    height: 260,
+  },
+  cardContent: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  cartActions: {
+    justifyContent: 'space-between',
+  },
+  buttons: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+};
+
+export default useStyles;
