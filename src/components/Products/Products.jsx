aa89a3f6990b28bd000ead1d309dd49aa6329fd2
Edward Chung <edwardchung2024@gmail.com>
2022-04-20 03:28:44 -0400

    filter categories fix

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Grid, Button, ThemeProvider, createTheme} from '@mui/material';
+import { commerce } from '../../lib/commerce';
 
 import Product from './Product/Product';
 import styles from './styles';
@@ -7,6 +8,7 @@ import useClasses from '../../hook';
 import block from '../../assets/block.png';
 
 const Products = ({ products, onAddToCart }) => {
+    const [category, setCategory] = useState("");
     const customTheme = createTheme({
         palette: {
           primary: {
@@ -14,8 +16,22 @@ const Products = ({ products, onAddToCart }) => {
           }
         }
     });
-    console.log(products);
+    const listProducts = products.map((product) => (
+        <Grid item key ={product.id} xs = {12} sm = {6} md = {4} lg = {3}>
+            <Product product = {product} onAddToCart = {onAddToCart} />
+        </Grid>
+    ))
+    const handleShowCategory = async (filter) => {
+        if(filter === "") {
+            setCategory(products);
+        } 
+        else {
+            const { data } = await commerce.products.list({category_slug: filter})
+            setCategory(data);
+        }
+    }
     const classes = useClasses(styles);
+
     return (
         <>
         <img src={block} alt="block" className={classes.image}/>
@@ -23,17 +39,17 @@ const Products = ({ products, onAddToCart }) => {
                 <div className={classes.toolbar}>
                     <Grid container justifyContent="center">
                         <ThemeProvider theme={customTheme}>
-                            <Button variant="contained" sx={{mr: 2}}>All</Button>
-                            <Button variant="contained" sx={{mr: 2}}>Peripherals</Button>
-                            <Button variant="contained" sx={{mr: 2}}>Processors</Button>
-                            <Button variant="contained" sx={{mr: 2}}>GPUs</Button>
-                            <Button variant="contained" sx={{mr: 2}}>Motherboards</Button>
-                            <Button variant="contained" sx={{mr: 2}}>Coolers</Button>
+                            <Button variant="contained" sx={{mr: 2}} onClick={() => handleShowCategory("")}>All</Button>
+                            <Button variant="contained" sx={{mr: 2}} onClick={() => handleShowCategory("Peripherals")}>Peripherals</Button>
+                            <Button variant="contained" sx={{mr: 2}} onClick={() => handleShowCategory("Processors")}>Processors</Button>
+                            <Button variant="contained" sx={{mr: 2}} onClick={() => handleShowCategory("GPU")}>GPUs</Button>
+                            <Button variant="contained" sx={{mr: 2}} onClick={() => handleShowCategory("Motherboards")}>Motherboards</Button>
+                            <Button variant="contained" sx={{mr: 2}} onClick={() => handleShowCategory("Coolers")}>Coolers</Button>
                         </ThemeProvider>
                     </Grid>
                 </div>
                     <Grid container justifyContent="center" spacing={4}>
-                        {products.map((product) => (
+                        {category === "" ? listProducts : category.map((product) => (
                             <Grid item key ={product.id} xs = {12} sm = {6} md = {4} lg = {3}>
                                 <Product product = {product} onAddToCart = {onAddToCart} />
                             </Grid>
