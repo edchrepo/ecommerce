b059d3d213e3ab76019b478a283021774848e3f7
Edward Chung <edwardchung2024@gmail.com>
2022-05-05 18:38:11 -0400

    breakpoints added for img under nav

--- src/components/Products/styles.js
+++ src/components/Products/styles.js
@@ -8,6 +8,13 @@ const useStyles = {
   image: {
     width: '100%',
     flexGrow: 1,
+    position: 'relative',
+    [theme.breakpoints.down('lg')]: {
+      marginTop: '5%',
+    },
+    [theme.breakpoints.down('sm')]: {
+      marginTop: '15%',
+    },
   },
   toolbar: theme.mixins.toolbar,
   content: {
